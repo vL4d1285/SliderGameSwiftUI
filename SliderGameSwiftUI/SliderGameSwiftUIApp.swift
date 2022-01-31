5470841417d0c85ad8939754c8d0fec32c760c99
Vladislav Ryabtsev <vryabtsev@me.com>
2022-01-31 18:13:03 +0300

    Initial Commit

---
+++ SliderGameSwiftUI/SliderGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameSwiftUIApp.swift
+//  SliderGameSwiftUI
+//
+//  Created by Vladislav Ryabtsev on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
